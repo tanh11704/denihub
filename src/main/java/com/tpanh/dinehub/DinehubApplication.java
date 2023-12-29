e8f6815026862feadea1d8b810a87dadbcff7fef
= <=>
2023-12-30 00:07:56 +0700

    Add initial project files

---
+++ src/main/java/com/tpanh/dinehub/DinehubApplication.java
@@ -0,0 +1,13 @@
+package com.tpanh.dinehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tpanh/dinehub/DinehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpanh.dinehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
